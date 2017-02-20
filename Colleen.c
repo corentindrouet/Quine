816ad9513e059bf85d6cc311fad1e41149c2e665
Corentin <corentin.drouet@hotmail.fr>
2017-02-20 13:47:48 +0100

    update nez line eof

--- Colleen.c
+++ Colleen.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /*
 	First comment
 */
@@ -12,6 +13,6 @@ int main()
 	/*
 		Second comment
 	*/
-	char *str="/*%c%cFirst comment%c*/%c%cvoid ok()%c{%c%creturn ;%c}%c%cint main()%c{%c%c/*%c%c%cSecond comment%c%c*/%c%cchar *str=%c%s%c;%c%cprintf(str, 10, 9, 10, 10, 10, 10, 10, 9, 10, 10, 10, 10, 10, 9, 10, 9, 9, 10, 9, 10, 9, 34, str, 34, 10, 9, 10);%c}";
-	printf(str, 10, 9, 10, 10, 10, 10, 10, 9, 10, 10, 10, 10, 10, 9, 10, 9, 9, 10, 9, 10, 9, 34, str, 34, 10, 9, 10);
+	char *str="#include <stdio.h>%c/*%c%cFirst comment%c*/%c%cvoid ok()%c{%c%creturn ;%c}%c%cint main()%c{%c%c/*%c%c%cSecond comment%c%c*/%c%cchar *str=%c%s%c;%c%cprintf(str, 10, 10, 9, 10, 10, 10, 10, 10, 9, 10, 10, 10, 10, 10, 9, 10, 9, 9, 10, 9, 10, 9, 34, str, 34, 10, 9, 10, 10);%c}%c";
+	printf(str, 10, 10, 9, 10, 10, 10, 10, 10, 9, 10, 10, 10, 10, 10, 9, 10, 9, 9, 10, 9, 10, 9, 34, str, 34, 10, 9, 10, 10);
 }
