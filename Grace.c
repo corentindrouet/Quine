081d29c8dfbd985131d55b0810b6a11e76a974d6
Corentin <corentin.drouet@hotmail.fr>
2017-02-20 14:28:39 +0100

    update grace

--- Grace.c
+++ Grace.c
@@ -2,5 +2,8 @@
 #include <fcntl.h>
 #define OPEN(x) fopen(x, "w")
 #define STRING() char*
-#define FT() int main(){STRING() str="#include <stdio.h>%c#include <fcntl.h>%c#define OPEN(x) fopen(x, %cw%c)%c#define STRING() char*%c#define FT() int main(){STRING() str=%c%s%c;FILE *kid=OPEN(%cGrace_kid.c%c);if(kid)fprintf(kid, str, 10, 10, 34, 34, 10, 10, 34, str, 34, 34, 34, 10, 10);}%cFT()%c";FILE *kid=OPEN("Grace_kid.c");if(kid)fprintf(kid, str, 10, 10, 34, 34, 10, 10, 34, str, 34, 34, 34, 10, 10);}
+#define FT() int main(){STRING() str="#include <stdio.h>%c#include <fcntl.h>%c#define OPEN(x) fopen(x, %cw%c)%c#define STRING() char*%c#define FT() int main(){STRING() str=%c%s%c;FILE *kid=OPEN(%cGrace_kid.c%c);if(kid)fprintf(kid, str, 10, 10, 34, 34, 10, 10, 34, str, 34, 34, 34, 10, 10, 10, 9, 10, 10);}%cFT()%c/*%c%cComment%c*/%c";FILE *kid=OPEN("Grace_kid.c");if(kid)fprintf(kid, str, 10, 10, 34, 34, 10, 10, 34, str, 34, 34, 34, 10, 10, 10, 9, 10, 10);}
 FT()
+/*
+	Comment
+*/
