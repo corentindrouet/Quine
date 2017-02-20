5b5388d8a8557a419dc6deda590c108032d89edd
Corentin <corentin.drouet@hotmail.fr>
2017-02-20 14:25:54 +0100

    Grace

---
+++ Grace.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <fcntl.h>
+#define OPEN(x) fopen(x, "w")
+#define STRING() char*
+#define FT() int main(){STRING() str="#include <stdio.h>%c#include <fcntl.h>%c#define OPEN(x) fopen(x, %cw%c)%c#define STRING() char*%c#define FT() int main(){STRING() str=%c%s%c;FILE *kid=OPEN(%cGrace_kid.c%c);if(kid)fprintf(kid, str, 10, 10, 34, 34, 10, 10, 34, str, 34, 34, 34, 10, 10);}%cFT()%c";FILE *kid=OPEN("Grace_kid.c");if(kid)fprintf(kid, str, 10, 10, 34, 34, 10, 10, 34, str, 34, 34, 34, 10, 10);}
+FT()
